a68a73d023d2e6ced222bb59810526d3bf020393
AndresAleu <andresaleunz@gmail.com>
2023-04-24 16:33:05 +0200

    Initial Commit

---
+++ JustLook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustLook
+//
+//  Created by Andres Aleu on 24/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
