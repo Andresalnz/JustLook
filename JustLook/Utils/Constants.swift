7b553d87b8a82e82d4eba306486569641a92496a
AndresAleu <andresaleunz@gmail.com>
2023-04-28 21:03:20 +0200

    Add constants

---
+++ JustLook/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  JustLook
+//
+//  Created by Andres Aleu on 28/4/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let TMDuri: String = "https://api.themoviedb.org/3/"
+    static let apiKey: String? = Util.getApiKey()
+}
