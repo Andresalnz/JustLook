3cbb01131cd448ad1cf4f8412efc8f3474869c8c
AndresAleu <andresaleunz@gmail.com>
2023-04-28 21:04:25 +0200

    Edit request

--- JustLook/Layers/Popular/Interactor/PopularMovieInteractor.swift
+++ JustLook/Layers/Popular/Interactor/PopularMovieInteractor.swift
@@ -10,27 +10,19 @@ import Alamofire
 
 final class PopularMovieInteractor {
     func getPopularMovies(url: String, completion: @escaping (Result<[ResultMovieDTO], Error>) -> Void) {
-        AF.request("https://api.themoviedb.org/3/movie/popular?api_key=d24265d9cbfd2e4119f0539b474f7c64&language=en-US&page=1", method: .get).response { response in
+        AF.request("https://api.themoviedb.org/3/movie/popular?api_key=d24265d9cbfd2e4119f0539b474f7c64&language=en-US&page=1", method: .get).responseDecodable(of: PopularMovieDTO.self) { response in
             if let error = response.error {
                 print("error, \(error)")
             }
+            
             guard let data = response.data else {
-                
                 completion(.failure(NSError(domain: "No data or invalid response", code: -1, userInfo: nil)))
                 return
             }
             
-            do {
-                let parseJsonPopularFilms = try JSONDecoder().decode(PopularMovieDTO.self, from: data)
-                
-                if let results = parseJsonPopularFilms.results {
-                    completion(.success(results))
-                }
-            }catch {
-                print(error.localizedDescription)
-                completion(.failure(error))
+            if let movies = response.value?.results {
+                completion(.success(movies))
             }
-            
         }
     }
 }
