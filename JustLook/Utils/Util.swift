65e9712a3e3c4deacaa95db1f1ecf67300c870e0
AndresAleu <andresaleunz@gmail.com>
2023-04-28 22:59:00 +0200

    Add method to shape a url

--- JustLook/Utils/Util.swift
+++ JustLook/Utils/Util.swift
@@ -10,11 +10,26 @@ import Foundation
 
 final class Util {
     
+    //MARK: - Obtener apikey de info.plist
     static func getApiKey() -> String? {
         let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
         guard let key = apiKey else {
             return "No existe"
         }
         return key
+    }
+    
+    //MARK: - Formar URL segun peticion
+    enum Services {
+        case popularMovie
+        
+        func shapeURL() ->URL? {
+            let apiKey = Util.getApiKey()!
+            let uri = Constants.TMDuri
+            switch self {
+            case .popularMovie:
+                return URL(string: "\(uri)movie/popular?api_key=\(String(describing: apiKey))&language=en-US&page=1")
+            }
+        }
     }
 }
