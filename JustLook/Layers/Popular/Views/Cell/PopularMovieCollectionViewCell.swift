276e6e4512244c1c196537bfe7cc701be4caefa9
AndresAleu <andresaleunz@gmail.com>
2023-05-02 12:33:37 +0200

    Add cell

---
+++ JustLook/Layers/Popular/Views/Cell/PopularMovieCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PopularMovieCollectionViewCell.swift
+//  JustLook
+//
+//  Created by Andres Aleu on 2/5/23.
+//
+
+import UIKit
+
+class PopularMovieCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier: String = "PopularCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- JustLook/Layers/Popular/Views/ListMovies/PopularMovieViewController.swift
+++ JustLook/Layers/Popular/Views/ListMovies/PopularMovieViewController.swift
@@ -19,6 +19,14 @@ class PopularMovieViewController: UIViewController {
         super.viewDidLoad()
         presenter?.viewDidLoad()
         styles()
+        setUpCollection()
+    }
+    
+    //MARK: - setUpCollection
+    func setUpCollection() {
+        self.collectionPopularMovies.delegate = self
+        self.collectionPopularMovies.dataSource = self
+        self.collectionPopularMovies.register(UINib(nibName:"PopularMovieCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: PopularMovieCollectionViewCell.identifier)
     }
     
     //MARK: - Styles
@@ -30,6 +38,23 @@ class PopularMovieViewController: UIViewController {
 extension PopularMovieViewController: PopularMovieUI {
     func update(moview: ResultMovieBO) {
         print(moview)
+    }
+}
+
+//MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension PopularMovieViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell: PopularMovieCollectionViewCell = collectionPopularMovies.dequeueReusableCell(withReuseIdentifier: PopularMovieCollectionViewCell.identifier, for: indexPath) as? PopularMovieCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
     }
 }
 
