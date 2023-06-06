a0b6639954a46a1450f4427dbe216df79ed74ffb
AndresAleu <andresaleunz@gmail.com>
2023-06-06 13:23:57 +0200

    Add Presenter and protocols

--- JustLook/Layers/Detail/DetailProtocols.swift
+++ JustLook/Layers/Detail/DetailProtocols.swift
@@ -7,6 +7,31 @@
 
 import Foundation
 
+//Protocolo del Interactor
 protocol DetailInteractor: AnyObject {
     func getDetailMovieOrSerie<T:Codable>(url: URL?, type: T.Type, completion: @escaping (Result<T, Error>) -> Void)
+}
+
+protocol DetailPresentable: AnyObject {
+    var ui: DetailUI? { get }
+    var detailInteractor: DetailInteractor { get }
+    var id: Int? { get }
+    var detailMovie: DetailMovieBO? { get }
+}
+
+//Protocolo presenter - Interactor
+protocol DetailPresenterInteractor : AnyObject {
+    func loadDetail()
+}
+
+//Protocolo Presenter - View
+protocol DetailUI: AnyObject {
+    func update(detailMovie: DetailMovieBO? )
+}
+
+//Extenison para colocar parametros por defectos
+extension DetailUI {
+    func update (detailMovie: DetailMovieBO? = nil ) {
+        update(detailMovie: detailMovie)
+    }
 }
---
+++ JustLook/Layers/Detail/Presenter/DetailPresenter.swift
@@ -0,0 +1,38 @@
+//
+//  DetailPresenter.swift
+//  JustLook
+//
+//  Created by Andres Aleu on 6/6/23.
+//
+
+import Foundation
+
+final class DetailPresenter: DetailPresentable {
+    weak var ui: DetailUI?
+    let detailInteractor: DetailInteractor
+    let id: Int?
+    var detailMovie: DetailMovieBO?
+    
+    init(id: Int?, detailInteractor: DetailInteractor) {
+        self.detailInteractor = detailInteractor
+        self.id = id
+    }
+}
+
+//MARK: - DetailPresenterInteractor
+extension DetailPresenter: DetailPresenterInteractor {
+    func loadDetail() {
+        detailInteractor.getDetailMovieOrSerie(url: Util.Services.DetailMovie.shapeURL(id: id), type: DetailMovieDTO.self) { [ weak self] data in
+            guard let wSelf = self else { return }
+            switch data {
+                case .success(let response):
+                    guard let detailMovieBO = response.toBo() else  { return }
+                    wSelf.detailMovie = detailMovieBO
+                    wSelf.ui?.update(detailMovie: wSelf.detailMovie)
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+        }
+    }
+  
+}
