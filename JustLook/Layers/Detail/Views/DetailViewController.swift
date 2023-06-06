a0774fa9eaefd7a47d9cad11ada612f23e28859b
AndresAleu <andresaleunz@gmail.com>
2023-06-06 13:41:44 +0200

    Add View

---
+++ JustLook/Layers/Detail/Views/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  JustLook
+//
+//  Created by Andres Aleu on 6/6/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
