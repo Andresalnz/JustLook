7ff7170c661e9395a5c6333ae9a70f42a7f23b48
AndresAleu <andresaleunz@gmail.com>
2023-06-07 12:29:21 +0200

    Add view

--- JustLook/Layers/Detail/Views/DetailViewController.swift
+++ JustLook/Layers/Detail/Views/DetailViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
+class DetailViewController: UIViewController, DetailUI {
+    
+    //MARK: - Variables
+     var presenter: DetailPresenterInteractor?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        presenter?.loadDetail()
+    
     }
-
+   
+    
+    func update(detailMovie: DetailMovieBO?) {
+        
+    }
+    
+ 
 }
