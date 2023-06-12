1fe55acd92458336d0751308b53e2f75653d9fa4
Andresalnz <andresaleunz@gmail.com>
2023-06-12 12:30:08 +0200

    Remove Table

--- JustLook/Layers/Detail/Views/DetailViewController.swift
+++ JustLook/Layers/Detail/Views/DetailViewController.swift
@@ -10,38 +10,17 @@ import UIKit
 class DetailViewController: UIViewController, DetailUI {
     
     //MARK: - IBOutlet
-    @IBOutlet weak var detailTableView: UITableView!
     
     //MARK: - Variables
-     var presenter: DetailUIPresenter?
+    var presenter: DetailUIPresenter?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.viewDidLoad()
-        setUpTableView()
+       
     }
-    
-    private func setUpTableView() {
-        detailTableView.delegate = self
-        detailTableView.dataSource = self
-    }
-   
     
     func update(detailMovie: DetailMovieBO?) {
         
     }
-}
-
-//MARK: - UITableViewDelegate, UITableViewDataSource
-extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        return UITableViewCell()
-    }
-    
-    
 }
