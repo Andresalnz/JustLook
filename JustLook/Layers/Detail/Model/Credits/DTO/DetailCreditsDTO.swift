f8d33828b39b5bc5d012acf71d128849390a5771
Andresalnz <andresaleunz@gmail.com>
2023-06-12 19:39:27 +0200

    Edit model

--- JustLook/Layers/Detail/Model/Credits/DTO/DetailCreditsDTO.swift
+++ JustLook/Layers/Detail/Model/Credits/DTO/DetailCreditsDTO.swift
@@ -6,3 +6,75 @@
 //
 
 import Foundation
+
+struct DetailCreditMoviesDTO: Codable {
+    let id: Int?
+    let result: [DetailCreditsActMoviesDTO]?
+    let personal: [DetailPersonalMoviesDTO]?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case result = "cast"
+        case personal = "crew"
+    }
+}
+
+struct DetailCreditsActMoviesDTO: Codable {
+    let adult: Bool?
+    let gender, id: Int?
+    let knownForDepartment: Department?
+    let name: String?
+    let originalName: String?
+    let popularity: Double?
+    let profilePath: String?
+    let castID: Int?
+    let character: String?
+    let creditID: String?
+    let order: Int?
+   
+    
+    enum CodingKeys: String, CodingKey {
+            case adult
+            case gender
+            case id
+            case knownForDepartment = "known_for_department"
+            case name
+            case originalName = "original_name"
+            case popularity
+            case profilePath = "profile_path"
+            case castID = "cast_id"
+            case character
+            case creditID = "credit_id"
+            case order
+        }
+}
+
+struct DetailPersonalMoviesDTO: Codable {
+    let adult: Bool?
+    let gender: Int?
+    let id: Int?
+    let knownForDepartment: Department?
+    let name: String?
+    let originalName: String?
+    let popularity: Double?
+    let profilePath: String?
+    let creditID: String
+    let order: Int?
+    let department: Department?
+    let job: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case adult, gender, id
+        case knownForDepartment = "known_for_department"
+        case name
+        case originalName = "original_name"
+        case popularity
+        case profilePath = "profile_path"
+        case creditID = "credit_id"
+        case order
+        case department
+        case job
+    }
+}
+
+
--- JustLook/Layers/Detail/Model/Credits/Mapper/DetailCreditsMapperBO.swift
+++ JustLook/Layers/Detail/Model/Credits/Mapper/DetailCreditsMapperBO.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+extension DetailCreditsActMoviesDTO {
+    func toBo() -> DetailCreditsActMoviesBO {
+       return DetailCreditsActMoviesBO(adult: adult,
+                                gender: gender,
+                                id: id,
+                                knownForDepartment: knownForDepartment,
+                                name: name,
+                                originalName: originalName,
+                                popularity: popularity,
+                                profilePath: URL(string: profilePath ?? ""),
+                                castID: castID,
+                                character: character,
+                                creditID: creditID,
+                                order: order)
+    }
+}
+
+extension DetailPersonalMoviesDTO {
+    func toBo() -> DetailPersonalMoviesBO {
+        return DetailPersonalMoviesBO(adult: adult,
+                                      gender: gender,
+                                      id: id,
+                                      knownForDepartment: knownForDepartment,
+                                      name: name,
+                                      originalName: originalName,
+                                      popularity: popularity,
+                                      profilePath: URL(string: profilePath ?? ""),
+                                      creditID: creditID,
+                                      order: order,
+                                      department: department,
+                                      job: job)
+    }
+}
