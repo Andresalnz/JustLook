188fc4e62c8bc1fc83594d0ca2bbd562acf0eb69
Andresalnz <andresaleunz@gmail.com>
2023-06-29 18:09:23 +0200

    Remove Button

--- JustLook/Layers/Popular/Views/ListMovies/PopularMovieViewController.swift
+++ JustLook/Layers/Popular/Views/ListMovies/PopularMovieViewController.swift
@@ -14,7 +14,6 @@ class PopularMovieViewController: UIViewController {
     
     //Buttons
     @IBOutlet weak var viewButtons: UIView!
-    @IBOutlet weak var btnAll: UIButton!
     @IBOutlet weak var btnSeries: UIButton!
     @IBOutlet weak var btnMovies: UIButton!
     
@@ -29,7 +28,7 @@ class PopularMovieViewController: UIViewController {
     //MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.btnAll.isSelected = true
+        self.btnMovies.isSelected = true
         presenter?.viewDidLoad()
         styles()
         setUpCollection()
@@ -58,7 +57,6 @@ class PopularMovieViewController: UIViewController {
         self.title = "Populares"
       
         //buttons
-        self.btnAll.generalButton("Todo", tintColor: .black)
         self.btnMovies.generalButton("Peliculas", tintColor: .black)
         self.btnSeries.generalButton("Series", tintColor: .black)
         
@@ -68,22 +66,9 @@ class PopularMovieViewController: UIViewController {
     }
     
     //MARK: - Actions
-    @IBAction func btnAllAction(_ sender: Any) {
-        self.btnSeries.isSelected = false
-        self.btnMovies.isSelected = false
-        self.btnAll.isSelected = true
-        presenter?.page = 1
-        if presenter?.movies.count == 0 {
-            presenter?.loadPopularMovies()
-        }
-        self.update()
-    }
-    
-    
     @IBAction func btnSeriesAction(_ sender: Any) {
         self.btnSeries.isSelected = true
         self.btnMovies.isSelected = false
-        self.btnAll.isSelected = false
         presenter?.page = 1
         if presenter?.series.count == 0 {
             presenter?.loadPopularSeries()
@@ -94,7 +79,6 @@ class PopularMovieViewController: UIViewController {
     @IBAction func btnMovieAction(_ sender: Any) {
         self.btnSeries.isSelected = false
         self.btnMovies.isSelected = true
-        self.btnAll.isSelected = false
         presenter?.page = 1
         if presenter?.movies.count == 0 {
             presenter?.loadPopularMovies()
@@ -125,9 +109,9 @@ extension PopularMovieViewController: UICollectionViewDelegate, UICollectionView
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         let btnMovie = self.btnMovies.isSelected
         let btnSerie = self.btnSeries.isSelected
-        let btnAll = self.btnAll.isSelected
+       
         
-        if btnMovie || btnAll {
+        if btnMovie {
             guard let items =  presenter?.movies.count else { return 0 }
             guard let items1 =  presenter?.searchAll.count else { return 0 }
             return  presenter!.filter ? items1 : items
@@ -154,7 +138,7 @@ extension PopularMovieViewController: UICollectionViewDelegate, UICollectionView
         
         let btnMovie = self.btnMovies.isSelected
         let btnSerie = self.btnSeries.isSelected
-        let btnAll = self.btnAll.isSelected
+        
         
         if  presenter?.filter == true && (presenter?.searchAll.count)! > 0 {
             if let item = presenter?.searchAll[indexPath.row] {
@@ -164,7 +148,7 @@ extension PopularMovieViewController: UICollectionViewDelegate, UICollectionView
                 presenter?.page += 1
                 presenter?.loadSearchMovies(searchText: nil)
             }
-        } else  if  btnMovie || btnAll {
+        } else  if  btnMovie {
             if let item = presenter?.movies[indexPath.row] {
                 cell.paintCell(item: item)
             }
